593906b6c46fbb4dcb3330e0431094436e79cdec
ardvansomeren <ard.van.someren@gmail.com>
2021-11-15 08:42:46 +0100

    Changed ttyACMx
    Have to change the serial port regulary - everytime RPi and Arduino are disconected the number can in or decrease.

--- write_serial.py
+++ write_serial.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM2',9600, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0',9600, timeout=1)
     ser.flush()
     
     while True:
