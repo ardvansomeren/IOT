d7fc8f27c46cfe7cf40c93bf285c0e92d83e30f1
ardvansomeren <ard.van.someren@gmail.com>
2021-11-16 14:07:13 +0100

    Added the Web Based Fan Switch project
    User story AB#98 and task AB#97. Besides these controller and backend parts,
    the project contains a PHP frontend. Located in /var/www/html. Until this
    moment not sure how to organize the version control of this front end and
    how to keep it in sync with the back end developments. Two possible strategies to consider:
     - Create one big repository
     - Create a separate repo for the front end

---
+++ temp_WebFanApp.py
@@ -0,0 +1,31 @@
+# AVS141 11/15/21 AB#97
+
+import serial
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM1',9600, timeout=1)
+    ser.flush()
+    
+    while True:
+
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            tempFile = open("/var/www/html/data.txt","w")
+            tempFile.write(line)
+            tempFile.close()
+            
+            print("***")
+
+            fanFile = open("/var/www/html/fanStatus.txt","r")
+            command = fanFile.read()
+            fanFile.close()
+            
+            print(line)
+            if command == 'on':
+                ser.write(b"on\n")
+                print(command)
+            elif command == 'off':
+                ser.write(b"off\n")
+                print(command)
+            else:
+                print("error")
